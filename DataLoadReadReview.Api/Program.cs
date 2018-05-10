3aeabd5fc320fdbd096b8ffade5e5da13cc15f14
tri-chandra <tri-chandra@users.noreply.github.com>
2018-05-10 11:10:48 +1000

    Kestrel starts on port 5001 and 5002

--- DataLoadReadReview.Api/Program.cs
+++ DataLoadReadReview.Api/Program.cs
@@ -20,6 +20,7 @@ namespace DataLoadReadReview.Api
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:5001")
                 .Build();
     }
 }
--- DataLoadReadReview.Web/Program.cs
+++ DataLoadReadReview.Web/Program.cs
@@ -20,6 +20,7 @@ namespace DataLoadReadReview.Web
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:5002")
                 .Build();
     }
 }
